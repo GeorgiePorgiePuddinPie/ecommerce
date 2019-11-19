d6049091ed7f38042adc039f938b03fd5adb1bdf
Douglas Friedline <40300447+douglasfriedline@users.noreply.github.com>
2019-11-19 16:27:30 -0600

    finished up shopping cart

--- insert_SC.php
+++ insert_SC.php
@@ -6,8 +6,7 @@
     $product = $Units = $TotalPrice = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $userId = $_SESSION["userId"];
-        echo "<br> $userId";
-        echo "<br> " . $_SESSION["userId"];
+
         // $userId = "10";
         $orderNumber = date("Y/m/d");
         if (empty($_POST["Products"])) {
@@ -32,26 +31,54 @@
         $data = htmlspecialchars($data);
         return $data;
     }
-    $sql="INSERT INTO cs3320.orders (userId, ordernumber, productID, Quantity, totalPrice)
-    VALUES
-    ('$userId', '$orderNumber', '$product', '$Units', '$TotalPrice')";
-    echo $sql;
     $conn = mysqli_connect('localhost','root','','cs3320');
     if (!$conn){    
         die('Could not connect: ' . mysql_error());
     }
-    echo "Connected successfully";
+
+    $result = mysqli_query($conn,"SELECT * FROM cs3320.orders WHERE userId ='".$userId."' AND orderNumber ='".$orderNumber."' AND productID ='".$product."'");
+
     foreach($errors as $msg) {
         echo "$msg <br>";
     }
-    // execute insert
-    if (!mysqli_query($conn,$sql))
-    {
-        die('Error: ' . mysqli_error($conn));
+    if (mysqli_num_rows($result)==0) {
+        // execute insert
+        $sql = "INSERT INTO cs3320.orders (userId, ordernumber, productID, Quantity, totalPrice)
+        VALUES ('".$userId."', '".$orderNumber."', '".$product."', '".$Units."', '".$TotalPrice."')";
+        mysqli_query($conn,$sql);
     }
     else {
-        echo "<br>1 record added";
+        if($Units == 0){
+            $sqlDelete = "DELETE FROM cs3320.orders WHERE userId ='".$userId."' AND orderNumber ='".$orderNumber."' AND productID ='".$product."'";
+            mysqli_query($conn,$sqlDelete);
+        }
+        else {
+            // execute update
+            $sql2 = "Update cs3320.orders SET Quantity ='".$Units."', totalPrice = '".$TotalPrice."' WHERE userId ='".$userId."' AND orderNumber = '".$orderNumber."' AND productID = '".$product."'";
+            mysqli_query($conn,$sql2);
+        }
     }
+    $result2 = mysqli_query($conn,"SELECT * FROM cs3320.orders WHERE userId ='".$userId."' AND orderNumber ='".$orderNumber."'");    
+    while($row = mysqli_fetch_assoc($result2)) {
+        $Prod = $row["productID"];
+        switch($Prod) {
+            case "2":
+                $Prod = "Apple";
+                break;
+            case "3":
+                $Prod = "Banana";
+                break;    
+            case "4":
+                $Prod= "Cantalope";
+                break;
+            case "5":
+                $Prod = "Dates";
+                break;
+        }
+        echo "<br>" .$Prod. " " .$row["Quantity"]. " Pieces, for a total of " .$row["totalPrice"]. ".";
+    }
+
+    
 mysqli_close($conn);
 ?>
 
